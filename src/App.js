2b6e7b3f27ef7cb05ae7bb2dce348daaaaec6df7
Ivens <ivensarroxellas@hotmail.com>
2022-08-01 14:51:55 -0300

    req 1

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, Switch, Link } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import ProfileEdit from './pages/ProfileEdit';
 import Profile from './pages/Profile';
 import Favorites from './pages/Favorites';
