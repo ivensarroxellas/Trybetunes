5ded1d48ef44425ab491c12c240d8027f78f5aac
Ivens <ivensarroxellas@hotmail.com>
2022-08-02 18:25:51 -0300

    req 6

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../component/Header';
+import searchAlbumsAPI from '../services/searchAlbumsAPI';
 
 class Search extends Component {
   state= {
     isButtonDisabled: true,
     artistName: '',
+    searchedAlbum: [],
+    loading: false,
+    ready: false,
+    artistReceived: '',
   }
 
   handleChange = (event) => {
@@ -16,14 +22,30 @@ class Search extends Component {
     const { artistName } = this.state;
     const dois = 2;
     const button = artistName.length < dois;
-    console.log(button);
     this.setState({
       isButtonDisabled: button,
     });
   }
 
+  onClicked = async (event) => {
+    event.preventDefault();
+    const { artistName } = this.state;
+    this.setState({
+      loading: true,
+    });
+    const response = await searchAlbumsAPI(artistName);
+    this.setState({
+      loading: false,
+      ready: true,
+      artistReceived: artistName,
+      artistName: '',
+      searchedAlbum: response,
+    });
+  }
+
   render() {
-    const { isButtonDisabled, artistName } = this.state;
+    const { isButtonDisabled, artistName, searchedAlbum, loading,
+      ready, artistReceived } = this.state;
     return (
       <>
         <Header />
@@ -42,9 +64,33 @@ class Search extends Component {
           type="button"
           data-testid="search-artist-button"
           disabled={ isButtonDisabled }
+          onClick={ this.onClicked }
         >
           Pesquisar
         </button>
+        {loading && <span>Carregando...</span>}
+        {ready && <h1>{`Resultado de álbuns de: ${artistReceived}`}</h1> }
+        { ready && searchedAlbum.length === 0 && <h1>Nenhum álbum foi encontrado</h1> }
+        <ul>
+          {searchedAlbum.map((artist, index) => (
+            <li key={ index }>
+              <h4>{artist.artistId}</h4>
+              <h2>{artist.artistName}</h2>
+              <h3>{artist.collectionId}</h3>
+              <h2>{artist.collectionName}</h2>
+              <h3>{artist.collectionPrice}</h3>
+              <img src={ artist.artworkUrl100 } alt="teste" />
+              <h2>{artist.releaseDate}</h2>
+              <h3>{artist.trackCount}</h3>
+              <Link
+                to={ `/album/${artist.collectionId}` }
+                data-testid={ `link-to-album-${artist.collectionId}` }
+              >
+                Música
+              </Link>
+            </li>
+          ))}
+        </ul>
       </>
     );
   }
