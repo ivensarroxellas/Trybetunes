b292600deabf9c694e46200e3c5b64265163a28c
Ivens <ivensarroxellas@hotmail.com>
2022-08-03 18:23:27 -0300

    req 8

--- src/component/MusicCard.js
+++ src/component/MusicCard.js
@@ -1,56 +1,22 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { addSong } from '../services/favoriteSongsAPI';
+import MusicDetails from './MusicDetails';
 
 class MusicCard extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      loading: false,
-    };
-  }
-
-  checkedItem = (music) => {
-    this.setState({
-      loading: true,
-    }, (() => {
-      addSong(music.trackId).then(() => {
-        this.setState({
-          loading: false,
-        });
-      });
-    }));
-  }
-
   render() {
     const { musicsList } = this.props;
-    const { loading } = this.state;
     return (
       <div>
-        { loading && <h1>Carregando...</h1> }
         <ul>
           {musicsList.map((music, index) => {
             if (index) {
               return (
-                <div key={ music.trackId }>
-                  <li>{music.trackName}</li>
-                  <audio data-testid="audio-component" src={ music.previewUrl } controls>
-                    <track kind="captions" />
-                    O seu navegador não suporta o elemento
-                    {' '}
-                    {' '}
-                    <code>audio</code>
-                    .
-                  </audio>
-                  <label htmlFor="favorite">
-                    <input
-                      id="favorite"
-                      type="checkbox"
-                      data-testid={ `checkbox-music-${music.trackId}` }
-                      onClick={ () => this.checkedItem(music) }
-                    />
-                  </label>
-                </div>
+                <MusicDetails
+                  key={ music.trackId }
+                  trackName={ music.trackName }
+                  previewUrl={ music.previewUrl }
+                  trackId={ music.trackId }
+                />
               );
             }
             return null;
---
+++ src/component/MusicDetails.js
@@ -0,0 +1,69 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { addSong } from '../services/favoriteSongsAPI';
+
+class MusicDetails extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      loading: false,
+      isChecked: false,
+    };
+  }
+
+  checkedItem = (id) => {
+    const { isChecked } = this.state;
+    if (!isChecked) {
+      this.setState({ isChecked: true });
+    } else {
+      this.setState({ isChecked: false });
+    }
+    this.setState({
+      loading: true,
+    }, (() => {
+      addSong(id).then(() => {
+        this.setState({
+          loading: false,
+        });
+      });
+    }));
+  }
+
+  render() {
+    const { loading, isChecked } = this.state;
+    const { trackName, previewUrl, trackId } = this.props;
+    return (
+      <>
+        { loading && <h1>Carregando...</h1> }
+        <div>
+          <li>{trackName}</li>
+          <audio data-testid="audio-component" src={ previewUrl } controls>
+            <track kind="captions" />
+            O seu navegador não suporta o elemento
+            {' '}
+            {' '}
+            <code>audio</code>
+            .
+          </audio>
+          <label htmlFor="favorite">
+            <input
+              id="favorite"
+              type="checkbox"
+              data-testid={ `checkbox-music-${trackId}` }
+              onChange={ () => this.checkedItem(trackId) }
+              checked={ isChecked }
+            />
+          </label>
+        </div>
+      </>
+    );
+  }
+}
+
+MusicDetails.propTypes = {
+  trackName: PropTypes.string.isRequired,
+  previewUrl: PropTypes.string.isRequired,
+  trackId: PropTypes.number.isRequired,
+
+};
+export default MusicDetails;
